22bdfcd6bacb2fa63d36996bfd6bc946fb4b6278
KunalK123 <70448227+KunalK123@users.noreply.github.com>
2020-09-15 19:33:40 -0400

    Create topic_subscriber.py

---
+++ topic_subscriber.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+import rospy
+from nav_msgs.msg import Odometry
+
+def callback(msg):
+    print msg.pose.pose
+
+rospy.init_node('topic_subscriber')
+sub = rospy.Subscriber('/odom', Odometry, callback)
+rospy.spin()
